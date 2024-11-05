f55ea694a96516e222d3c5e0a6a1c0895eeb0cca
nic-cc <nicolas.capello@carecentra.com>
2024-11-05 15:27:24 -0800

    Initial Commit

---
+++ Flim/FlimApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlimApp.swift
+//  Flim
+//
+//  Created by Nicolas Capello on 11/5/24.
+//
+
+import SwiftUI
+
+@main
+struct FlimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FlimTests/FlimTests.swift
@@ -0,0 +1,17 @@
+//
+//  FlimTests.swift
+//  FlimTests
+//
+//  Created by Nicolas Capello on 11/5/24.
+//
+
+import Testing
+@testable import Flim
+
+struct FlimTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
